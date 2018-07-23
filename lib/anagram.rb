a4559ee4f5286ca2456ec1d54598080912830b46
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-23 23:19:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,6 +9,7 @@ class Anagram
   
   def match(array)
     array.select {|w| w.split("").sort == @word.split("").sort}
-    # First it takes the array 
+    # First it takes the array and checks to see all the elements that will pass true in the block.
+    # Within the block it splits the individual words and sorts them. 
   end 
 end 
