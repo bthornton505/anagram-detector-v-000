737331023cbe30c577d39c5e728d90f5adfd0c93
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-23 23:18:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,6 @@ class Anagram
   end 
   
   def match(array)
-    array.select {|w| w.split(" ").sort == @word.split(" ").sort}
+    array.select {|w| w.split("").sort == @word.split("").sort}
   end 
 end 
